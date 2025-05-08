1c33b869fa8f53dbf1776737a8dd36e7622c9b0c
saqar-az <132606173+saqar-az@users.noreply.github.com>
2025-05-09 02:16:35 +0330

    commit hw_1

---
+++ hw_1.m
@@ -0,0 +1,48 @@
+clc
+clear
+
+% 1 : a 10*5 matrix : 
+A = [0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4
+    0 1 2 3 4];
+
+% 2 : a 1*25 matrix : 
+B = [0 1 2 3 4 5 6 7 8 9 ...
+    10 11 12 13 14 15 16 17 18 19 ...
+    20 21 22 23 24];
+
+% 3 : an array from -10 to 10 with an increment of 1 :
+C = -10 : 10;
+
+% 4 : an array from 10 to 1 with an decrement of 5 :
+D = 10 : -5 : 1;
+
+% 5 : an array from 0 to 2 with an increment of 0.5 :
+E = 0 : 0.5 : 2;
+
+% 6 : 5 equally spaced points from 1 to 10 :
+F = linspace(1,10,5);
+
+% 7 : 10 equally spaced points from -2 to 2 :
+G = linspace(-2,2,10);
+
+% 8 : 5 equally spaced points from 1 to 4 :
+H = logspace(1, 4, 5);
+
+% 9 : 10 equally spaced points from -1 to 1 :
+I = logspace(-1, 1, 10);
+
+% 10 : multiplier of number 5 in ranges (0, 30) :
+J = 5 : 5 :25;
+
+% 11 : multiplier of number 5 in ranges [0, 30] :
+K = 0 : 5 :30;
+
+
